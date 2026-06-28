a929dbd9f046c2d723b08043b4272ac463c70091
sumtie <154700677+sumtie@users.noreply.github.com>
2026-06-28 16:56:12 +0800

    fix SubtitleDialog

--- app/src/main/java/com/fongmi/android/tv/ui/dialog/SubtitleDialog.java
+++ app/src/main/java/com/fongmi/android/tv/ui/dialog/SubtitleDialog.java
@@ -69,23 +69,19 @@ public final class SubtitleDialog extends BaseDialog {
     }
 
     private void onUp(View view) {
-        subtitleView.addBottomPadding(0.005f);
-        Setting.putSubtitleBottomPadding(subtitleView.getBottomPadding());
+
     }
 
     private void onDown(View view) {
-        subtitleView.subBottomPadding(0.005f);
-        Setting.putSubtitleBottomPadding(subtitleView.getBottomPadding());
+
     }
 
     private void onLarge(View view) {
-        subtitleView.addTextSize(0.002f);
-        Setting.putSubtitleTextSize(subtitleView.getTextSize());
+
     }
 
     private void onSmall(View view) {
-        subtitleView.subTextSize(0.002f);
-        Setting.putSubtitleTextSize(subtitleView.getTextSize());
+
     }
 
     private void onReset(View view) {
